28efb90a43c59b09fb6581e90613ebba4d620829
João Rambo <jpramboguanabara@hotmail.com>
2022-07-18 20:17:38 -0300

    authentication with phantom and metamask

--- src/components/TabBox.tsx
+++ src/components/TabBox.tsx
@@ -1,107 +1,104 @@
-import { Tab } from '@headlessui/react'
 import { useState } from 'react'
+import { Tab } from '@headlessui/react'
 
 export default function TabBox() {
-    let [categories] = useState({
-        Recent: [
-          {
-            id: 1,
-            title: 'Does drinking coffee make you smarter?',
-            date: '5h ago',
-            commentCount: 5,
-            shareCount: 2,
-          },
-          {
-            id: 2,
-            title: "So you've bought coffee... now what?",
-            date: '2h ago',
-            commentCount: 3,
-            shareCount: 2,
-          },
-        ],
-        Popular: [
-          {
-            id: 1,
-            title: 'Is tech making coffee better or worse?',
-            date: 'Jan 7',
-            commentCount: 29,
-            shareCount: 16,
-          },
-          {
-            id: 2,
-            title: 'The most innovative things happening in coffee',
-            date: 'Mar 19',
-            commentCount: 24,
-            shareCount: 12,
-          },
-        ],
-        Trending: [
-          {
-            id: 1,
-            title: 'Ask Me Anything: 10 answers to your questions about coffee',
-            date: '2d ago',
-            commentCount: 9,
-            shareCount: 5,
-          },
-          {
-            id: 2,
-            title: "The worst advice we've ever heard about coffee",
-            date: '4d ago',
-            commentCount: 1,
-            shareCount: 2,
-          },
-        ],
-      })
+  let [categories] = useState({
+    Balance: [
+      {
+        id: 1,
+        title: 'Does drinking coffee make you smarter?',
+        date: '5h ago',
+        commentCount: 5,
+        shareCount: 2,
+      },
+      {
+        id: 2,
+        title: "So you've bought coffee... now what?",
+        date: '2h ago',
+        commentCount: 3,
+        shareCount: 2,
+      },
+    ],
+    Transactions: [
+      {
+        id: 1,
+        title: 'Ask Me Anything: 10 answers to your questions about coffee',
+        date: '2d ago',
+        commentCount: 9,
+        shareCount: 5,
+      },
+      {
+        id: 2,
+        title: "The worst advice we've ever heard about coffee",
+        date: '4d ago',
+        commentCount: 1,
+        shareCount: 2,
+      },
+    ],
+    NFTs: [
+      {
+        id: 1,
+        title: 'Is tech making coffee better or worse?',
+        date: 'Jan 7',
+        commentCount: 29,
+        shareCount: 16,
+      },
+      {
+        id: 2,
+        title: 'The most innovative things happening in coffee',
+        date: 'Mar 19',
+        commentCount: 24,
+        shareCount: 12,
+      },
+    ],
+    Send: [
+      {
+        id: 1,
+        title: 'Does drinking coffee make you smarter?',
+        date: '5h ago',
+        commentCount: 5,
+        shareCount: 2,
+      },
+      {
+        id: 2,
+        title: "So you've bought coffee... now what?",
+        date: '2h ago',
+        commentCount: 3,
+        shareCount: 2,
+      },
+    ],
+  })
 
-    
   return (
-    <div className="w-full max-w-md px-2 py-16 sm:px-0">
-    <Tab.Group>
-      <Tab.List className="flex space-x-1 rounded-xl bg-slate-900 p-1">
-        {Object.keys(categories).map((category) => (
-          <Tab
-            key={category}
-            className='w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-slate-200 hover:bg-slate-600 focus:bg-slate-700 focus:ring-slate-400 focus:ring'
-          >
-            {category}
-          </Tab>
-        ))}
-      </Tab.List>
-      <Tab.Panels className="mt-2">
-        {Object.values(categories).map((posts, idx) => (
-          <Tab.Panel
-            key={idx}
-            className='rounded-xl bg-white p-3'
+    <div className="w-full px-2 py-16 sm:px-0 mx-auto">
+      <Tab.Group>
+        <Tab.List className="flex space-x-1 rounded-xl p-1 bg-slate-900">
+          {Object.keys(categories).map((category) => (
+            <Tab
+              key={category}
+              className='w-full rounded-lg py-2.5 text-lg font-bold leading-5 text-slate-200 hover:bg-slate-700 transition duration-500 focus:ring-0.5 focus:bg-slate-700'                 
             >
-            <ul>
-              {posts.map((post) => (
-                <li
-                  key={post.id}
-                  className="relative rounded-md p-3 hover:bg-gray-100"
-                >
-                  <h3 className="text-sm font-medium leading-5">
-                    {post.title}
-                  </h3>
-
-                  <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
-                    <li>{post.date}</li>
-                    <li>&middot;</li>
-                    <li>{post.commentCount} comments</li>
-                    <li>&middot;</li>
-                    <li>{post.shareCount} shares</li>
-                  </ul>
-
-                  <a
-                    href="#"
-                    className='ring-blue-400 focus:z-10 focus:outline-none focus:ring-2'
-                  />
-                </li>
-              ))}
-            </ul>
-          </Tab.Panel>
-        ))}
-      </Tab.Panels>
-    </Tab.Group>
-  </div>
+              {category}
+            </Tab>
+          ))}
+        </Tab.List>
+        <Tab.Panels className="mt-2">
+          {Object.values(categories).map((posts, idx) => (
+            <Tab.Panel
+              key={idx}
+              className='w-full h-full bg-slate-400'
+            >
+                {posts.map((hehe) => {
+                  return (
+                    <div>
+                      {hehe.commentCount}
+                    </div>
+                  )
+                })}
+            </Tab.Panel>
+          ))}
+        </Tab.Panels>
+      </Tab.Group>
+    </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react"
+import { Fragment, useEffect, useState } from "react"
 import { useMoralis } from "react-moralis"
-import { RefreshIcon } from "@heroicons/react/solid"
+import { ChevronDownIcon, RefreshIcon } from "@heroicons/react/solid"
 import Head from "next/head"
 import Header from "../components/Header"
 import TabBox from "../components/TabBox"
+import { Menu, Transition } from '@headlessui/react'
+import metamask from '../public/metamask.png'
+import phantom from '../public/phantom.png'
 
 export default function Home() {
   const { 
@@ -31,13 +34,73 @@ export default function Home() {
           <h1 className="text-3xl md:text-5xl font-bold text-slate-300 flex mx-auto justify-center">
             Hello, web3
           </h1>
-          <button 
-            onClick={() => authenticate({
+          <Menu as="div" className="relative inline-block text-left">
+            <div>
+              <Menu.Button 
+                className="px-8 py-4 bg-slate-300 bg-gradient-to-r from-slate-500 rounded-xl flex justify-center font-bold w-[255px] md:w-[355px] hover:bg-slate-500 transition duration-300">
+                {isAuthenticating ? <RefreshIcon className="animate-spin w-8 h-8 text-slate-900" aria-hidden="true"/> : 'Connect your wallet'}
+                {!isAuthenticating && 
+                  <ChevronDownIcon
+                    className="ml-4 md:ml-8 -mr-1 h-6 w-6 text-slate-800 hover:text-violet-100"
+                    aria-hidden="true"
+                  />
+                }
+                
+              </Menu.Button>            
+            </div>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="w-[355px] absolute right-0 mt-2 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="px-1 py-1 space-y-2">
+                  <Menu.Item>
+                    <div 
+                      onClick={() => {
+                        authenticate({
+                          signingMessage: "Sign to log in to web3 dapp"
+                        })
+                      }} 
+                      className="flex items-center space-x-4 px-2 hover:bg-slate-500 transition duration-500 rounded-xl">
+                      <img src={metamask.src} alt="logo metamask" className="w-10 h-10"/>
+                      <button
+                        className='w-[320px] mx-auto text-black font-bold group flex w-full items-center rounded-xl px-2 py-4 text-sm'
+                      >
+                        Connect with Metamask
+                      </button>
+                    </div>
+                  </Menu.Item>
+                  <Menu.Item>
+                    <div 
+                      onClick={() => {
+                        authenticate({
+                          type: 'sol',
+                          signingMessage: "Sign to log in to web3 dapp" 
+                        })
+                      }}
+                      className="flex items-center space-x-4 px-2 hover:bg-slate-500 transition duration-500 rounded-xl">
+                      <img src={phantom.src} alt="logo phantom" className="w-10 h-10"/>
+                      <button
+                        className='w-[320px] mx-auto text-black font-bold group flex w-full items-center rounded-md px-2 py-4 text-sm'
+                      >
+                        Connect with Phantom
+                      </button>
+                    </div>
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+
+            </Transition>
+          </Menu>
+
+          {/* onClick={() => authenticate({
               signingMessage: "Sign to log in web3 app"
-            })}
-            className="px-8 py-4 bg-slate-300 bg-gradient-to-r from-slate-500 rounded-xl flex justify-center font-bold w-[255px] md:w-[355px] hover:bg-slate-500 transition duration-300">
-            {isAuthenticating ? <RefreshIcon className="animate-spin w-8 h-8 text-slate-900" aria-hidden="true"/> : 'Connect your wallet'}
-          </button>
+            })} */}
         </div>  
       </div>
       </>
@@ -55,7 +118,7 @@ export default function Home() {
           logout={logout}
           user={user}
           />
-          <div className="flex-1 w-full bg-slate-800 h-screen px-32 py-24">
+          <div className="flex-1 bg-slate-800 h-screen px-32 py-24">
             <TabBox />
           </div>
       </div>
