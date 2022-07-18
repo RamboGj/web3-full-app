3e99eeb3fe3191c0ef6aa5a3b050906a7fe604a5
João Rambo <jpramboguanabara@hotmail.com>
2022-07-18 12:33:56 -0300

    responsive login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,15 +26,15 @@ export default function Home() {
         <title>Login | web3</title>
       </Head>
       <div className="h-screen flex">
-        <div className="m-auto space-y-12">
-          <h1 className="text-5xl font-bold text-slate-300 flex mx-auto justify-center">
+        <div className="mx-auto my-32 md:m-auto space-y-12">
+          <h1 className="text-3xl md:text-5xl font-bold text-slate-300 flex mx-auto justify-center">
             Hello, web3
           </h1>
           <button 
             onClick={() => authenticate({
               signingMessage: "Sign to log in web3 app"
             })}
-            className="px-8 py-4 bg-slate-300 bg-gradient-to-r from-slate-500 rounded-xl flex justify-center font-bold w-[355px] hover:bg-slate-500 transition duration-300">
+            className="px-8 py-4 bg-slate-300 bg-gradient-to-r from-slate-500 rounded-xl flex justify-center font-bold w-[255px] md:w-[355px] hover:bg-slate-500 transition duration-300">
             {isAuthenticating ? <RefreshIcon className="animate-spin w-8 h-8 text-slate-900" aria-hidden="true"/> : 'Connect your wallet'}
           </button>
         </div>  
